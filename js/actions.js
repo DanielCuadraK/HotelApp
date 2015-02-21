d7c2da06a26dad0ee598433cdb85b2b5105a7118
dck26 <daniel.cuadra@gmail.com>
2015-02-21 13:51:43 -0600

    Primera versión
    primera versión

---
+++ js/actions.js
@@ -0,0 +1,9 @@
+var fn = {
+    init: function () {
+        var x = false;
+        if (!x)
+            window.location.href = "#registro";
+    }
+};
+
+window.addEventListener("load", fn.init, false);
