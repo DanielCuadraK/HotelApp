2aa3c170394f5011002bb8c3fdab5bcf379b8980
dck26 <daniel.cuadra@gmail.com>
2015-03-07 13:05:12 -0600

    version 2

--- js/actions.js
+++ js/actions.js
@@ -1,9 +1,27 @@
 var fn = {
+    ready: function(){
+        document.addEventListener('deviceready',fn.init,false); //device ready es una función de phonegap que avisa cuando ya se terminó de cargar todas las librerías
+    },
     init: function () {
         var x = false;
         if (!x)
             window.location.href = "#registro";
+        $('#btRegistrar').tap(fn.registrar);
+    },
+    registrar: function(){
+        //var nombre = $('#registro input:eq(0)').val();
+        //var email = $('#registro input:eq(1)').val();
+        //var tel = $('#registro input:eq(2)').val();
+        var nombre = $('#inNombre').val();
+        var email = $('#inEmail').val();
+        var tel = $('#inTel').val();
+        if(nombre != '' && email != '' && tel != '')
+            alert('todo bien');
+        else
+            navigator.notification.alert('Faltan de llenar algunos campos', null, 'Error de Datos', 'OK');
     }
 };
-
-window.addEventListener("load", fn.init, false);
+$(fn.ready);
+//window.addEventListener("load", fn.init, false); opción puro javascript
+//var nobj = $(document); opción jquery
+//nobj.ready(fn.init);
