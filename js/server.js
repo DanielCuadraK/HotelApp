f2987ea130011e15815037b3bd72fdddcdeb66ee
dck26 <daniel.cuadra@gmail.com>
2015-03-14 12:20:24 -0600

    v3.5

--- js/server.js
+++ js/server.js
@@ -4,14 +4,9 @@ var server = {
     url: 'http://carlos.igitsoft.com/apps/test.php',
     sendData: function(nom, mail, tel, img){
         $.mobile.loading('show');
-        $.ajax({
-            method: "POST",
-            url: server.url,
-            data: { nom: nom, mail: mail, tel: tel }
-            }).done(function( msg ) {
-            if(msg == 1) {
+        $.ajax({method: "POST",url: server.url,data: { nom: nom, mail: mail, tel: tel }}).done(function( msg ) {
+            if(msg == 1)
                 transfer.imgUpload(img);
-            }
         });
     
     }
