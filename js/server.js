99235018385b85366d60bb736465edd97ad813a2
dck26 <daniel.cuadra@gmail.com>
2015-03-14 12:28:37 -0600

    3.5

--- js/server.js
+++ js/server.js
@@ -1,13 +1,13 @@
 //Envío de datos al servidor
 
 var server = {
-    url: 'http://carlos.igitsoft.com/apps/test.php',
+    //url: 'http://carlos.igitsoft.com/apps/test.php',
     sendData: function(nom, mail, tel, img){
-        $.mobile.loading('show');
+        /*$.mobile.loading('show');
         $.ajax({method: "POST",url: server.url,data: { nom: nom, mail: mail, tel: tel }}).done(function( msg ) {
             if(msg == 1)
                 transfer.imgUpload(img);
-        });
-    
+        });*/
+    alert("hola mundo");
     }
 };
