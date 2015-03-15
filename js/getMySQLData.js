ab2ca01629343e04a06c9d3d7d435d6b34085234
dck26 <daniel.cuadra@gmail.com>
2015-03-15 01:41:35 -0600

    3

--- js/getMySQLData.js
+++ js/getMySQLData.js
@@ -2,25 +2,12 @@
 var sqlData = {
 	getSQLData: function(){
     var output = $('#output');
-    output.text('Prueba');
     alert('Si entrè');
-	$.ajax({
-        url: 'http://www.elterreno.com.mx/sicop/connect.php',
-		dataType: 'jsonp',
-		jsonp: 'jsoncallback',
-		timeout: 5000,
-		success: function(data, status){
-			$.each(data, function(i,item){ 
-				/*var movie = '<h1>'+item.idMovie+'</h1>'
-				+ '<p>'+item.OriginalTitle+'<br>'
-				+'</p>';*/
-				alert(item.OriginalTitle);
-				//output.append(movie);
-			});
-		},
-		error: function(){
-			output.text('There was an error loading the data.');
-		}
-	});
-}
+$.ajax({
+            url: 'http://www.elterreno.com.mx/sicop/connect.php',
+            dataType: 'json',
+        }).done(function( data ) {
+            alert(data.OriginalTitle[0]);
+        });
+    }
 };
