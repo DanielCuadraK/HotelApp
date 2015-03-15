511ae0167f8d84f63671af47f3d9a2525e374521
dck26 <daniel.cuadra@gmail.com>
2015-03-15 00:50:53 -0600

    v3

--- js/getMySQLData.js
+++ js/getMySQLData.js
@@ -2,16 +2,17 @@
 var sqlData = {
 	getSQLData: function(){
     var output = $('#output');
+    alert('Si entrè');
 	$.ajax({
-		url: 'http://www.elterreno.com.mx/sicop/connect.php',
+        url: 'http://www.elterreno.com.mx/sicop/connect.php',
 		dataType: 'jsonp',
 		jsonp: 'jsoncallback',
 		timeout: 5000,
 		success: function(data, status){
 			$.each(data, function(i,item){ 
-				var movie = '<h1>'+item.name+'</h1>'
-				+ '<p>'+item.latitude+'<br>'
-				+ item.longitude+'</p>';
+				var movie = '<h1>'+item.idMovie+'</h1>'
+				+ '<p>'+item.OriginalTitle+'<br>'
+				+'</p>';
 				
 				output.append(movie);
 			});
